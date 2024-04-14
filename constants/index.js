63c3aa1e5f9e98966b1c01b396ea7c2254b7c7b6
Ali Ahamed Thowfeek <alitheoneman@gmail.com>
2024-04-14 11:06:18 +0530

    add supported banks and currencies

---
+++ constants/index.js
@@ -0,0 +1,12 @@
+const supportedBanks = {
+  combank: { id: "combank", name: "Commercial Bank Of Ceylon" },
+};
+
+const supportedCurrencies = {
+  USD: { id: "USD", name: "United States Dollar", alternative: ["US DOLLARS"] },
+};
+
+module.exports = {
+  supportedBanks: supportedBanks,
+  supportedCurrencies: supportedCurrencies,
+};
